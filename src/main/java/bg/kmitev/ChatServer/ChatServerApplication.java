d66820550f6075893717d89f97b27ce711bf0773
Kaloyan <dm.kaloyan@gmail.com>
2021-12-05 21:34:38 +0200

    Spring boot initial project added

---
+++ src/main/java/bg/kmitev/ChatServer/ChatServerApplication.java
@@ -0,0 +1,13 @@
+package bg.kmitev.ChatServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bg/kmitev/ChatServer/ChatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.kmitev.ChatServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
